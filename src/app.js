3b264db17324a3944b3aec3b642856aef78c823b
Surya Sekhar Prajapati <113052765+Suryachamp@users.noreply.github.com>
2026-04-09 00:15:33 +0530

    Update registration logic in router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+
 app.use(express.json());
 
+// REQUIRE ALL THE ROUTES HERE
+const authRouter=require("./routes/auth.routes");
 
+
+// USING ALL THE ROUTES HERE
+app.use("/api/auth",authRouter); 
 
 module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/user.model");
+
+/**
+ * @name registerUserController
+ * @description Register a new user, expects username, email, password in request body
+ * @access public
+ */
+
+async function registerUserController(req, res) {
+
+}
+
+module.exports = {
+    registerUserController
+};
